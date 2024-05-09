5200e62a3e2363156db4de92c2b814f0de54e12f
Zehra Özer <119448653+Zwhraoz@users.noreply.github.com>
2024-05-09 18:40:44 +0300

    Initial Commit

---
+++ Uygulama_3_fotograf_Listesi/Uygulama_3_fotograf_ListesiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uygulama_3_fotograf_ListesiApp.swift
+//  Uygulama_3_fotograf_Listesi
+//
+//  Created by zehra özer on 9.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Uygulama_3_fotograf_ListesiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
